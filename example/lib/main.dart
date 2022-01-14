dfba89fa271de699af3dea6f952da78c24762bd5
KouT127 <kou26haru@gmail.com>
2022-01-14 23:23:20 +0900

    modify: Added hooks example

---
+++ example/lib/main.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+import 'dart:developer' as developer;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:http/http.dart' as http;
+import 'package:swr_requester_hooks/swr_requester_hooks.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends HookWidget {
+  const MyApp({Key? key}) : super(key: key);
+  final flutterPluginsRepositoryPath = "/repos/flutter/plugins";
+  final flutterRepositoryPath = "/repos/flutter/flutter";
+
+  @override
+  Widget build(BuildContext context) {
+    final requestPath = useState<String>(flutterRepositoryPath);
+    final response = useSWRRequest(requestPath.value, (path) async {
+      final uri = Uri.https("api.github.com", path);
+      developer.log(
+        "Request to ${uri.toString()}",
+      );
+      final result = await http.get(uri);
+      final Map<String, dynamic> json = jsonDecode(result.body);
+      return json;
+    });
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Example"),
+        ),
+        body: response.data == null
+            ? const Text("Loading")
+            : Center(
+                child: Text(
+                  response.data?["full_name"] ?? "",
+                ),
+              ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            requestPath.value = flutterRepositoryPath == requestPath.value
+                ? flutterPluginsRepositoryPath
+                : flutterRepositoryPath;
+          },
+        ),
+      ),
+    );
+  }
+}
