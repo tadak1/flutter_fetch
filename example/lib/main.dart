6bd5af06333bb1c32719d9470c3605b9eef3db61
KouT127 <kou26haru@gmail.com>
2022-01-16 20:38:50 +0900

    modify: Fixed to match the library method names.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_fetch_hooks/flutter_fetch_hooks.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:http/http.dart' as http;
+import "package:riverpod/riverpod.dart";
 
 void main() {
   runApp(const MyApp());
@@ -18,12 +19,13 @@ class MyApp extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final requestPath = useState<String>(flutterRepositoryPath);
-    final response = useSWRRequest(requestPath.value, (path) async {
+    final response = useFetch(requestPath.value, (path) async {
       final uri = Uri.https("api.github.com", path);
       developer.log(
         "Request to ${uri.toString()}",
       );
       final result = await http.get(uri);
+      await Future.delayed(const Duration(seconds: 2));
       final Map<String, dynamic> json = jsonDecode(result.body);
       return json;
     });
@@ -35,13 +37,21 @@ class MyApp extends HookWidget {
         appBar: AppBar(
           title: const Text("Example"),
         ),
-        body: response.data == null
-            ? const Text("Loading")
-            : Center(
-                child: Text(
-                  response.data?["full_name"] ?? "",
-                ),
+        body: response.when(
+          data: (response) {
+            return Center(
+              child: Text(
+                response?["full_name"] ?? "",
               ),
+            );
+          },
+          loading: () {
+            return const Text("Loading");
+          },
+          error: (Object error, StackTrace? stackTrace) {
+            return Text(error.toString());
+          },
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             requestPath.value = flutterRepositoryPath == requestPath.value
